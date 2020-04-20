e796230b2e43466f270f7007bc092e3f41f8da59
Henry Kyle <hkcoding@protonmail.com>
2020-04-20 13:12:48 +1200

    Update 001-tidy, add 002-tidy.

--- hhh4_001-tidy.R
+++ hhh4_001-tidy.R
@@ -2,70 +2,78 @@
 
 library(dplyr)
 library(tidyr)
-library(rvest)
-library(readxl)
-library(httr)
 library(stringr)
+library(lubridate)
+library(rvest)
+library(httr)
+library(readxl)
+library(janitor)
 
 # Scrape latest DataURL and DataFilename
 
-Page <- read_html("https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-current-situation/covid-19-current-cases/covid-19-current-cases-details")
+DataPage <- read_html("https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-current-situation/covid-19-current-cases/covid-19-current-cases-details")
 
-PageLinks <- Page %>%
+DataPageLinks <- DataPage %>%
   html_nodes("a") %>%
   html_attr("href")
 
-DataURLSuffix <- grep("system/files/documents/pages/", PageLinks, value = TRUE)
+DataURLSuffix <- grep("system/files/documents/pages/", DataPageLinks, value = TRUE)
 
 DataFilename <- sub(".+/", "", DataURLSuffix)
 
 DataURL <- paste0("https://www.health.govt.nz", DataURLSuffix)
 
-# Error check. Passing will download new data if available, otherwise use local. Result printed.
+# Error check. Passing will download new data if available, otherwise use local. Result printed
+
+DataDir <- "Data\\NZCases\\"
+DataPath <- paste0(DataDir, DataFilename)
 
 if (length(DataURL) == 1 & str_sub(DataFilename, -5, -1) == ".xlsx") {
-  if (any(list.files() == DataFilename)) {
-    print(paste0("Latest MoH Case Data already in current dir: ", DataFilename))
+  if (any(list.files(DataDir) == DataFilename)) {
+    print(paste0("Latest MoH Case Data already available locally: ", DataFilename))
   }
   else {
     print(paste0("Found new MoH Case Data: ", DataFilename))
-    GET(DataURL, write_disk(DataFilename), progress())
+    GET(DataURL, write_disk(paste0(DataDir, DataFilename)), progress())
   }
 } else {
   print("ERROR scraping latest Data. Verify checks below:")
   length(DataURL) == 1
   str_sub(DataFilename, -5, -1) == ".xlsx"
   DataFilename <- NULL
+  DataPath <- NULL
 }
 
 
-# Import Data, bind sheets, verify structure as expected
+# Import Data, bind sheets, clean ColNames, verify structure as expected
 
 ExpectedSheets <- c("Confirmed", "Probable")
-ExpectedCols <- c("Date of report", "Sex", "Age group", "DHB", "International travel", 
-                  "Last country before return", "Flight number", "Flight departure date", "Arrival date")
+ExpectedCols <- c("DateOfReport", "Sex", "AgeGroup", "DHB", "OverseasTravel", "LastCountryBeforeReturn", "FlightNumber", "FlightDepartureDate", "ArrivalDate")
 
-if(identical(excel_sheets(DataFilename), ExpectedSheets)) {
-  NZ_Covid19_Confirmed <- read_excel(DataFilename, sheet = "Confirmed", skip = 2)
-  NZ_Covid19_Probable <- read_excel(DataFilename, sheet = "Probable", skip = 2)
+if (identical(excel_sheets(DataPath), ExpectedSheets)) {
+  NZ_Covid19_Confirmed <- read_excel(DataPath, sheet = "Confirmed", skip = 3)
+  NZ_Covid19_Probable <- read_excel(DataPath, sheet = "Probable", skip = 3)
 } else {
   print("ERROR sheets exception. Received:")
-  print(excel_sheets(DataFilename))
+  print(excel_sheets(DataPath))
   print("Expected:")
   print(ExpectedSheets)
 }
 
-NZ_Covid19_All <- bind_rows(NZ_Covid19_Confirmed, NZ_Covid19_Probable)
+NZ_Covid19_All <- bind_rows(NZ_Covid19_Confirmed, NZ_Covid19_Probable) %>%
+  clean_names(., "upper_camel") %>%
+  rename(DHB = Dhb)
 
 if (identical(colnames(NZ_Covid19_All), ExpectedCols)) {
   print("PASSED data structure test")
 } else {
   print("ERROR column names exception:")
-  colnames(NZ_Covid19_all) == ExpectedCols
+  colnames(NZ_Covid19_All) == ExpectedCols
 }
 
+# Clean values, verify, select, aggregate, sort
 
-# Clean up, verify, select, aggregate sort
+####### Consider trimming white space:
 
 NZ_Covid19_All$DHB <- gsub(" ", "", NZ_Covid19_All$DHB)
 NZ_Covid19_All$DHB <- gsub("'", "", NZ_Covid19_All$DHB)
@@ -73,28 +81,47 @@ NZ_Covid19_All$DHB <- gsub("'", "", NZ_Covid19_All$DHB)
 ## TO DO: make below a test
 ## sort(unique(NZ_Covid19$DHB)
 
-NZ_Covid19_All$`Date of report` <- as.Date(NZ_Covid19_All$`Date of report`)
+NZ_Covid19_All$DateOfReport <- dmy(NZ_Covid19_All$DateOfReport)
 
 NZ_Covid19_Selected <- NZ_Covid19_All %>%
-  select("DHB", "Date of report")
+  select("DHB", "DateOfReport")
 
 NZ_Covid19_Selected_Agg <- NZ_Covid19_Selected %>%
-  dplyr::count(DHB, `Date of report`, name = "NewCases") %>%
-  arrange(DHB, `Date of report`)
+  count(DHB, DateOfReport, name = "NewCases") %>%
+  arrange(DHB, DateOfReport)
 
 # Create DateRange Tibble
 
-## Hope to eventually have a day with zero new cases. Set DateMax to data release date
-DateMax <- read_excel(DataFilename, sheet = "Confirmed", "A2")
-## Cheating for now, just need to format above
-DateMax <- max(NZ_Covid19_All$`Date of report`)
-DateMin <- min(NZ_Covid19_All$`Date of report`)
+## Need to discuss issues with what DateMax to use - Perhaps prudent to use cell A2 - 1 or 2 days.
+DateMax <- as.vector(read_excel(DataPath, sheet = "Confirmed", "A2", col_names = "Date")) %>%
+  pull(., Date) %>%
+  as_date(.)
+
+if (is.Date(DateMax)) {
+  print(paste0("Correctly scraped DateMax from cell A2 as: ", DateMax))
+} else {
+  DateMax <- max(NZ_Covid19_Selected_Agg$DateOfReport)
+  print(paste0("Unable to scrape DateMax from cell A2, using max(DateOfReport) instead: ", DateMax))
+}
+
+DateMin <- min(NZ_Covid19_All$DateOfReport)
 
 DateRange <- tibble(Date = seq.Date(DateMin, DateMax, by = "day"))
 
-# Join, clean
+# Join, clean, pivot wider
 
-NZ_Covid19 <- full_join(NZ_Covid19_Selected_Agg, DateRange, by = c("Date of report" = "Date")) %>%
-  complete(DHB, nesting(`Date of report`), fill = list(NewCases = 0)) %>%
-  filter(!is.na(DHB))
+NZ_Covid19 <- full_join(NZ_Covid19_Selected_Agg, DateRange, by = c("DateOfReport" = "Date")) %>%
+  complete(DHB, nesting(DateOfReport), fill = list(NewCases = 0)) %>%
+  filter(!is.na(DHB)) %>%
+  group_by(DHB) %>%
+  mutate(CumsumCases = cumsum(NewCases))
 
+# Finalizing for compatibility hhh4_002: nz_counts_t
+
+nz_counts_t <- NZ_Covid19 %>%
+  select(-NewCases) %>%
+  pivot_wider(., names_from = DHB, values_from = CumsumCases) %>%
+  mutate(DateOfReport = as.numeric(DateOfReport) - 18317) %>%
+  rename(seq_dayte = DateOfReport)
+
+## Optional: clear unneeded objects: rm(list=setdiff(ls(), "nz_counts_t"))
---
+++ hhh4_002-tidy.R
@@ -0,0 +1,50 @@
+# As this step is static data end to end, consider saving outputs and removing this from the standard flow.
+
+library(dplyr)
+library(readxl)
+library(surveillance)
+library(sf)
+library(stringr)
+library(spdep)
+
+# Pull map file, remove Z co-ordinates, fix ordering
+
+dhb_shp_2012 <- st_read("Data\\GeospatialData\\DHB2012\\nz-district-health-boards-2012.shp") %>%
+  st_zm(.) %>%
+  arrange(DHB12)
+
+dhb_shp_2012 %>%
+  arrange(DHB12)
+
+# Clean DHB name, verify
+
+dhb_shp_2012$NAME <- dhb_shp_2012$NAME %>%
+  str_replace_all(c(" |'" = "", "Hutt" = "HuttValley", "Midcentral" = "MidCentral"))
+
+ExpectedDHBs <- read_excel("Data\\StandardisedNames.xlsx", sheet = "DHB") %>%
+  select(DHB)
+
+identical(dhb_shp_2012$NAME, ExpectedDHBs$DHB)
+
+# Create R Map
+
+map_2012 <- as(dhb_shp_2012, "Spatial")
+## Optional visual check: plot(map_2012)
+
+# Calc adjacency matrix and nbOrder matrix
+
+nzrems_adjmat_2012 <- poly2adjmat(map_2012, row.names = ExpectedDHBs$DHB)
+
+colnames(nzrems_adjmat_2012) <- ExpectedDHBs$DHB
+
+nzrems_nbOrder_2012 <- nbOrder(nzrems_adjmat_2012, maxlag=Inf)
+
+## Temporary: Checks against values from hhh4_002-CompareOutputs.R
+identical(nzrems_adjmat_2012, nzrems_adjmat)
+identical(nzrems_nbOrder_2012, nzrems_nbOrder)
+## Haha. At least our plots will look better.
+
+# Finalizing. Maintaining _2012 on name for now
+nz_counts_t_002_2012 <- nz_counts_t %>%
+  select(-seq_dayte) %>%
+  select(order(ExpectedDHBs$DHB, decreasing = TRUE))
